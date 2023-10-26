c1e621b816f0f7a44335f2df59c04814c7b30b53
omkar930 <93o68644o9@gmail.com>
2023-10-26 12:27:36 +0530

    added_comments

--- scripts/game/game.js
+++ scripts/game/game.js
@@ -1,111 +1,125 @@
-
-
-
+// Select the canvas element and get its 2D rendering context
 const canvas = document.querySelector('canvas');
 console.log("omkar");
 const c = canvas.getContext('2d');
+
+// Set the canvas size to match the window dimensions
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+// Initialize a variable to track points
 var points = 0;
 
 //...........................................
 
-function Player(x,y,radius,color){
+// Define a constructor for the player character
+function Player(x, y, radius, color) {
     this.x = x;
     this.y = y;
     this.radius = radius;
     this.color = color;
 
-    this.draw = function(){
-        c.arc(this.x,this.y,this.radius,0,Math.PI*2,false);
+    // Method to draw the player character
+    this.draw = function () {
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
         c.fillStyle = this.color;
         c.fill();
     }
-    
 }
 
 //................................................
 
-function Projectile(x,y,radius,color,velocity){
+// Define a constructor for projectiles
+function Projectile(x, y, radius, color, velocity) {
     this.x = x;
     this.y = y;
     this.radius = radius;
     this.color = color;
     this.velocity = velocity;
 
-    this.draw = function(){
+    // Method to draw a projectile
+    this.draw = function () {
         c.beginPath();
-        c.arc(this.x,this.y,this.radius,0,Math.PI*2,false);
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
         c.fillStyle = this.color;
         c.fill();
     }
-    this.update = function(){
+
+    // Method to update the position of the projectile
+    this.update = function () {
         this.draw();
-        this.x  = this.x + this.velocity.x;
-        this.y  = this.y + this.velocity.y;
+        this.x = this.x + this.velocity.x;
+        this.y = this.y + this.velocity.y;
     }
 }
 
 //........................................................
 
-function Enemy(x,y,radius,color,velocity){
+// Define a constructor for enemies
+function Enemy(x, y, radius, color, velocity) {
     this.x = x;
     this.y = y;
     this.radius = radius;
     this.color = color;
     this.velocity = velocity;
 
-    this.draw = function(){
+    // Method to draw an enemy
+    this.draw = function () {
         c.beginPath();
-        c.arc(this.x,this.y,this.radius,0,Math.PI*2,false);
-   
-        
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
         c.fillStyle = color;
-        c.strokeStyle='black';
+        c.strokeStyle = 'black';
         c.lineWidth = 2;
         c.fill();
-        c.stroke()
-        
-        
+        c.stroke();
     }
-    this.update = function(){
+
+    // Method to update the position of the enemy
+    this.update = function () {
         this.draw();
-        this.x  = this.x + this.velocity.x;
-        this.y  = this.y + this.velocity.y;
+        this.x = this.x + this.velocity.x;
+        this.y = this.y + this.velocity.y;
     }
 }
 
 //....................................................
 
-function Particle(x,y,radius,color,velocity){
+// Define a constructor for particles
+function Particle(x, y, radius, color, velocity) {
     this.x = x;
     this.y = y;
     this.radius = radius;
     this.color = color;
     this.velocity = velocity;
 
-    this.draw = function(){
+    // Method to draw a particle
+    this.draw = function () {
         this.radius -= 0.2;
         c.beginPath();
-        c.arc(this.x,this.y,this.radius,0,Math.PI*2,false);
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
         c.fillStyle = color;
         c.fill();
-
-        
     }
-    this.update = function(){
+
+    // Method to update the position of the particle
+    this.update = function () {
         this.draw();
-        this.x  = this.x + this.velocity.x;
-        this.y  = this.y + this.velocity.y;
+        this.x = this.x + this.velocity.x;
+        this.y = this.y + this.velocity.y;
     }
 }
 
 //...................................................
 
-const player = new Player(canvas.width/2,canvas.height/2,10,"black");
+// Create an instance of the player character
+const player = new Player(canvas.width / 2, canvas.height / 2, 10, "black");
+
+// Create arrays to hold projectiles, enemies, and particles
 const projectiles = [];
-const ememies = [];
+const enemies = [];
 const particles = [];
+
+// Define an array of colors for enemies
 var colScheme = [
     "#264653",
     "#2A9D8F",
@@ -114,105 +128,134 @@ var colScheme = [
     "#E76F51"
 ]
 
-function spawnEnemies(){
-    setInterval(()=>{
-        const radius = Math.random()*(30-4)+4;
+// Function to continuously spawn enemies
+function spawnEnemies() {
+    setInterval(() => {
+        const radius = Math.random() * (30 - 4) + 4;
         let x;
         let y;
-        if(Math.random() < 0.5){
-            x = (Math.random() < 0.5) ? 0-radius*2 : canvas.width+radius*2;
-            y = Math.random()*canvas.height+radius*2;
-        }else{
-            x = Math.random()*canvas.width+radius*2;
-            y = (Math.random() < 0.5) ? 0-radius*2 : canvas.height+radius*2;
+        if (Math.random() < 0.5) {
+            x = (Math.random() < 0.5) ? 0 - radius * 2 : canvas.width + radius * 2;
+            y = Math.random() * canvas.height + radius * 2;
+        } else {
+            x = Math.random() * canvas.width + radius * 2;
+            y = (Math.random() < 0.5) ? 0 - radius * 2 : canvas.height + radius * 2;
         }
-        
-        
-       // const color = `hsl(${Math.random()*360},50%,50%)`;
-        const color = colScheme[Math.floor(Math.random()*5)]
-        const angle = Math.atan2(canvas.height/2 - y,canvas.width/2-x);
+
+        // Choose a random color for the enemy
+        const color = colScheme[Math.floor(Math.random() * 5)]
+
+        // Calculate the angle and velocity for the enemy
+        const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x);
         const velocity = {
             x: Math.cos(angle),
-            y: Math.sin(angle)    }
-        ememies.push(new Enemy(x,y,radius,color,velocity));
-    },1000);
+            y: Math.sin(angle)
+        }
+
+        // Create a new enemy and add it to the enemies array
+        enemies.push(new Enemy(x, y, radius, color, velocity));
+    }, 1000);
 }
 
+// Variable to store the animation frame ID
 let animationId;
-function animate(){
+
+// Animation loop function
+function animate() {
     animationId = requestAnimationFrame(animate);
-    
-    c.clearRect(0,0,canvas.width,canvas.height);
-    document.getElementById('overlay').innerText=points;
+
+    // Clear the entire canvas
+    c.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Update and draw the player character
     player.draw();
-    particles.forEach((item,index)=>{
-        if(item.radius < 1){
-            particles.splice(index,1);
+
+    // Update and draw the particles
+    particles.forEach((item, index) => {
+        if (item.radius < 1) {
+            particles.splice(index, 1);
         }
         item.update();
-    })
-    projectiles.forEach((projectile,pIndex)=>{
+    });
+
+    // Update and draw the projectiles
+    projectiles.forEach((projectile, pIndex) => {
         projectile.update();
-        if(projectile.x + projectile.radius < 0 ||
-            projectile.x-projectile.radius > canvas.width ||
+
+        // Remove projectiles that go out of the canvas
+        if (projectile.x + projectile.radius < 0 ||
+            projectile.x - projectile.radius > canvas.width ||
             projectile.y + projectile.radius < 0 ||
-            projectile.y - projectile.radius > canvas.height){
-            setTimeout(()=>{
-                projectiles.splice(pIndex,1);
-                },0)
+            projectile.y - projectile.radius > canvas.height) {
+            setTimeout(() => {
+                projectiles.splice(pIndex, 1);
+            }, 0)
         }
     });
-    ememies.forEach((enemy,eIndex)=>{
+
+    // Update and draw the enemies
+    enemies.forEach((enemy, eIndex) => {
         enemy.update();
 
-        const dist = Math.hypot(player.x - enemy.x,player.y - enemy.y);
-        if(dist - enemy.radius - player.radius < 1){
-            let final = {pts : points};
-            localStorage.setItem("score010",JSON.stringify(final));
+        // Check for collision with the player character
+        const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y);
+        if (dist - enemy.radius - player.radius < 1) {
+            // Game over logic
+            let final = { pts: points };
+            localStorage.setItem("score010", JSON.stringify(final));
             cancelAnimationFrame(animationId);
-            window.location.href="../pages/end.html";
-            
+            window.location.href = "../pages/end.html";
         }
-        projectiles.forEach((projectile,pIndex) =>{
-            const dist = Math.hypot(projectile.x - enemy.x,projectile.y - enemy.y);
-            if(dist - enemy.radius - projectile.radius < 1){
-                for(let i = 0;i<6;i++){
-                    particles.push(new Particle(projectile.x,projectile.y,6,enemy.color,{
+
+        // Check for collision with projectiles
+        projectiles.forEach((projectile, pIndex) => {
+            const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y);
+            if (dist - enemy.radius - projectile.radius < 1) {
+                // Create particles for the explosion
+                for (let i = 0; i < 6; i++) {
+                    particles.push(new Particle(projectile.x, projectile.y, 6, enemy.color, {
                         x: Math.random() - 0.5,
-                        y: Math.random()-0.5
+                        y: Math.random() - 0.5
                     }))
                 }
-                if(enemy.radius > 20){
+
+                // Reduce the enemy's radius and remove the projectile
+                if (enemy.radius > 20) {
                     enemy.radius -= 10;
-                    
-                    setTimeout(()=>{
-                        projectiles.splice(pIndex,1);
-                        points+=10;
-                    },0)
-                }else{
-                    
-                    setTimeout(()=>{
-                        ememies.splice(eIndex,1);
-                        projectiles.splice(pIndex,1);
-                        points+=10;
-                    },0)
+                    setTimeout(() => {
+                        projectiles.splice(pIndex, 1);
+                        points += 10;
+                    }, 0)
+                } else {
+                    // Remove the enemy and the projectile
+                    setTimeout(() => {
+                        enemies.splice(eIndex, 1);
+                        projectiles.splice(pIndex, 1);
+                        points += 10;
+                    }, 0)
                 }
             }
         })
     })
-    
 }
 
-window.addEventListener('click',(event)=>{
-    console.log(projectiles);
-    const angle = Math.atan2(event.clientY - canvas.height/2,event.clientX - canvas.width/2);
+// Add a click event listener to allow the player to shoot projectiles
+window.addEventListener('click', (event) => {
+    // Calculate the angle and velocity for the projectile
+    const angle = Math.atan2(event.clientY - canvas.height / 2, event.clientX - canvas.width / 2);
     const velocity = {
-        x: Math.cos(angle)*5,
-        y: Math.sin(angle)*5    
+        x: Math.cos(angle) * 5,
+        y: Math.sin(angle) * 5
     }
-    points-=2;
-    projectiles.push(new Projectile(canvas.width/2,canvas.height/2,3,"black",velocity ))
+
+    // Subtract points when firing a projectile
+    points -= 2;
+
+    // Create a new projectile and add it to the projectiles array
+    projectiles.push(new Projectile(canvas.width / 2, canvas.height / 2, 3, "black", velocity))
 })
 
+// Start the game animation loop and enemy spawning
 animate();
 spawnEnemies();
+
--- scripts/home/home.js
+++ scripts/home/home.js
@@ -1,23 +1,29 @@
+// Select the canvas element and get its 2D rendering context
 var canvas = document.querySelector('canvas');
 
+// Set the canvas size to match the window dimensions
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 var c = canvas.getContext('2d');
-var mouse  = {
+
+// Define an object to store the mouse coordinates
+var mouse = {
     x: undefined,
     y: undefined
 }
 
+// Define the maximum and minimum radii for circles
 var maxRadius = 40;
 var minRadius = 4;
 
-window.addEventListener('mousemove',
-function(event){
+// Event listener for tracking mouse movement
+window.addEventListener('mousemove', function(event) {
     mouse.x = event.x;
     mouse.y = event.y;
 })
 
+// Define an array of colors for circles
 var colScheme = [
     "#264653",
     "#2A9D8F",
@@ -26,7 +32,8 @@ var colScheme = [
     "#E76F51"
 ]
 
-function Circle(x,y,dx,dy,radius,color){
+// Constructor function for creating circles
+function Circle(x, y, dx, dy, radius, color) {
     this.x = x;
     this.y = y;
     this.dx = dx;
@@ -35,56 +42,70 @@ function Circle(x,y,dx,dy,radius,color){
     this.color = color;
     this.minRadius = radius;
 
-    this.draw = function(){
+    // Method to draw a circle
+    this.draw = function() {
         c.beginPath();
-        c.arc(this.x,this.y,this.radius,0,Math.PI*2,false);
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
         c.fillStyle = this.color;
         c.fill();
     }
-    this.update = function(){
-        if(this.x > innerWidth-this.radius || this.x < this.radius){
+
+    // Method to update the circle's position and size
+    this.update = function() {
+        // Bounce off the canvas edges
+        if (this.x > innerWidth - this.radius || this.x < this.radius) {
             this.dx = -this.dx;
         }
-        if(this.y > innerHeight-this.radius || this.y<this.radius){
+        if (this.y > innerHeight - this.radius || this.y < this.radius) {
             this.dy = -this.dy;
         }
-        this.x+=this.dx;
-        this.y+=this.dy;
-        //interactivity
-        if(mouse.x-this.x < 50 && mouse.x-this.x>-50
-            && mouse.y-this.y < 50 && mouse.y-this.y>-50) {
-                if(this.radius<maxRadius){
-                    this.radius+=1;
-                }
-                
-        }else if(this.radius > this.minRadius){
+        this.x += this.dx;
+        this.y += this.dy;
+
+        // Interactive resizing based on mouse proximity
+        if (mouse.x - this.x < 50 && mouse.x - this.x > -50 &&
+            mouse.y - this.y < 50 && mouse.y - this.y > -50) {
+            if (this.radius < maxRadius) {
+                this.radius += 1;
+            }
+        } else if (this.radius > this.minRadius) {
             this.radius--;
         }
+
         this.draw();
     }
 }
 
+// Create an array to store multiple circle objects
 var circleArray = [];
 
-for(var i = 0;i<150;i++){
+// Generate and push random circles into the array
+for (var i = 0; i < 150; i++) {
     var radius = Math.random() * 8 + 8;
-    var color = colScheme[Math.floor(Math.random()*5)];
-    var x = Math.random() * (innerWidth - radius*2) + radius;
-    var y = Math.random() * (innerHeight- radius*2) + radius;
+    var color = colScheme[Math.floor(Math.random() * 5)];
+    var x = Math.random() * (innerWidth - radius * 2) + radius;
+    var y = Math.random() * (innerHeight - radius * 2) + radius;
     var dx = (Math.random() - 0.5) * 4;
-    var dy = (Math.random() - 0.5)* 4;
-    
-    circleArray.push(new Circle(x,y,dx,dy,radius,color));
+    var dy = (Math.random() - 0.5) * 4;
+
+    circleArray.push(new Circle(x, y, dx, dy, radius, color));
 }
+
 console.log(circleArray);
 
-function animate(){
+// Animation function
+function animate() {
     requestAnimationFrame(animate);
-    c.clearRect(0,0,innerWidth,innerHeight);
-    for(var i = 0;i<circleArray.length;i++){
+
+    // Clear the canvas
+    c.clearRect(0, 0, innerWidth, innerHeight);
+
+    // Update and draw all circles in the array
+    for (var i = 0; i < circleArray.length; i++) {
         circleArray[i].update();
     }
 }
-animate();
 
+// Start the animation loop
+animate();
 
